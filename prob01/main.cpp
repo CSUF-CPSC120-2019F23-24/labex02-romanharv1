50adbbcf195c0d13c1e5da3322e5d2e63b57d7c7
romanharv1 <romanharv1@cs104u-4a.acad.fullerton.edu>
2019-08-31 13:38:07 -0700

    Created diamond pattern output

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,17 @@
-// Name: 
+// Name: Diamond Pattern
 // This program displays a diamond pattern to the screen.
 
+// Matching sample output
 #include <iostream>
 
 int main()
 {
-
+std::cout << "   *\n";
+std::cout << "  ***\n";
+std::cout << " *****\n";
+std::cout << "*******\n";
+std::cout << " *****\n";
+std::cout << "  ***\n";
+std::cout << "   *\n";
   return 0;
 }
